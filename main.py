0b3175c7014fb428e0f1b429ecae79c21412a8cf
Aayush Raj <aayush616@gmail.com>
2024-02-02 12:07:24 -0700

    Move functional code to function

--- main.py
+++ main.py
@@ -14,47 +14,42 @@ import pandas as pd
 import requests
 import time
 
-# debug auto-quit
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-
-service = Service(executable_path="/Users/aakritisuresh/anaconda3/main.python.selenium/chromedriver-mac-x64/chromedriver")
-driver = webdriver.Chrome(options=options, service=service)
-
-# open url
-url = "https://cels.baltimorehousing.org/Search_DEM_Map.aspx"
-#ABELLE url = "https://cels.baltimorehousing.org/TL_DEM_Map.aspx"
-#ALLENDALE url = "https://cels.baltimorehousing.org/TL_DEM_Map.aspx"
-#ARCADIA url = "https://cels.baltimorehousing.org/TL_DEM_Map.aspx"
-base_url = driver.get(url)
-
-# select all neighbourhood placeholder
-select_element = Select(driver.find_element("id","ctl00_ContentPlaceHolder1_lstLoc"))
-
-# count the number of neighbourhoods 
-all_neighbourhoods = select_element.options
-print(len(all_neighbourhoods))
-
-# select Neighborhood(by value
-#ABELL = select_element.select_by_value("ABELL")
-
-# select Neighbourhood(by Index)
-select_element.select_by_index(1)
-
-# extract all neighbourhood names
-#for neighborhood in all_neighbourhoods:
-#    print(neighborhood.text)
-
-# select Search Button
-search_button = driver.find_element('id','ctl00_ContentPlaceHolder1_btNB')  
-search_button.click()
-
-# url for new page
-# response=driver.page_source
-
-
 # extract table contents
 def main():
+    # debug auto-quit
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+
+    service = Service(executable_path="/Users/aakritisuresh/anaconda3/main.python.selenium/chromedriver-mac-x64/chromedriver")
+    driver = webdriver.Chrome(options=options, service=service)
+
+    # open url
+    url = "https://cels.baltimorehousing.org/Search_DEM_Map.aspx"
+    #ABELLE url = "https://cels.baltimorehousing.org/TL_DEM_Map.aspx"
+    #ALLENDALE url = "https://cels.baltimorehousing.org/TL_DEM_Map.aspx"
+    #ARCADIA url = "https://cels.baltimorehousing.org/TL_DEM_Map.aspx"
+    base_url = driver.get(url)
+
+    # select all neighbourhood placeholder
+    select_element = Select(driver.find_element("id","ctl00_ContentPlaceHolder1_lstLoc"))
+
+    # count the number of neighbourhoods
+    all_neighbourhoods = select_element.options
+    print(len(all_neighbourhoods))
+
+    # select Neighborhood(by value
+    #ABELL = select_element.select_by_value("ABELL")
+
+    # select Neighbourhood(by Index)
+    select_element.select_by_index(1)
+
+    # extract all neighbourhood names
+    #for neighborhood in all_neighbourhoods:
+    #    print(neighborhood.text)
+
+    # select Search Button
+    search_button = driver.find_element('id','ctl00_ContentPlaceHolder1_btNB')
+    search_button.click()
     tbody = driver.find_element('xpath', '//*[@id="ctl00_ContentPlaceHolder1_DataGrid1"]')
     data = []
     for tr in tbody.find_elements(By.XPATH, '//tr'):
